80ae619d6e9ced4f841f09672c5e4cb9ad0ed5cf
Rajveer Singh <138320694+rajveersingh086@users.noreply.github.com>
2024-09-22 01:17:46 +0530

    Add files via upload

---
+++ insertend.java
@@ -0,0 +1,38 @@
+package geeks.linked_list;
+
+class Node{
+    int data ;
+    Node next;
+    Node (int x){
+        data =x;
+        next= null;
+    }
+}
+public class insertend {
+    static Node insertEnd(Node head , int x){
+        Node temp = new Node(x);
+        if(head == null){     //if is empty
+        return temp;
+        }
+        Node curr = head;
+        while (curr.next!=null){
+        curr=curr.next;
+        }
+        curr.next = temp;
+        return head;
+    }
+    public static void main(String[] args) {
+        Node head = null;
+        head = insertEnd(head, 10);
+        head = insertEnd(head, 20);
+        head = insertEnd(head, 30);
+        printlist(head);
+    }
+    public static void printlist (Node head ){
+        Node curr = head ;
+        while (curr != null){
+            System.out.println(curr.data + " ");
+            curr = curr.next;
+        }
+    }
+}
